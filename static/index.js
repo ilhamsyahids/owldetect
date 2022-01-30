9d6d6355a121e1ceb3d65657397b83654680448b
Ilham Syahid S <ilhamsyahids@gmail.com>
2022-01-30 23:38:49 +0700

    marking the output with color

--- static/index.js
+++ static/index.js
@@ -3,21 +3,41 @@ async function analyzeText() {
     const inputText = document.getElementById("inputText");
     const refText = document.getElementById("refText");
 
+    const output = document.getElementById("output");
+
+    const inputTextValue = inputText.value;
+    const refTextValue = refText.value;
+
+    const divInputEl = document.createElement("div");
+    divInputEl.innerHTML = inputTextValue;
+    divInputEl.classList.add("output-item");
+
+    const divRefEl = document.createElement("div");
+    divRefEl.innerHTML = refTextValue;
+    divRefEl.classList.add("output-item");
+
+    output.appendChild(divInputEl);
+    output.appendChild(divRefEl);
+
     const response = await fetch("/analysis", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "input_text": inputText.value,
-            "ref_text": refText.value,
+            "input_text": inputTextValue,
+            "ref_text": refTextValue,
         })
     });
 
     const apiResp = await response.json();
-    if (apiResp.data.matches) {
+    if (apiResp.data.matches.length > 0) {
+        divInputEl.classList.add("red");
+        divRefEl.classList.add("red");
         alert("plagiarism found!");
     } else {
+        divInputEl.classList.add("green");
+        divRefEl.classList.add("green");
         alert("no plagiarism");
     }
 }
