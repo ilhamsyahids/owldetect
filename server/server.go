bbadf0ec76ff15402dda2da5e685585df7e32449
Ilham Syahid S <ilhamsyahids@gmail.com>
2022-02-02 14:59:49 +0700

    refactor server

--- server/server.go
+++ server/server.go
@@ -29,36 +29,38 @@ func HomeRoot() http.Handler {
 	return http.FileServer(http.Dir("./static"))
 }
 
+func AnalysisRoot(w http.ResponseWriter, r *http.Request) {
+	// check http method
+	if r.Method != http.MethodPost {
+		api.WriteAPIResp(w, api.NewErrorResp(errors.NewErrMethodNotAllowed()))
+		return
+	}
+
+	// parse request body
+	var reqBody api.AnalyzeReqBody
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		api.WriteAPIResp(w, api.NewErrorResp(errors.NewErrBadRequest(err.Error())))
+		return
+	}
+
+	// validate request body
+	err = reqBody.Validate()
+	if err != nil {
+		api.WriteAPIResp(w, api.NewErrorResp(err))
+		return
+	}
+
+	// do analysis
+	matches := plagiarism.Analyze(reqBody.InputText, reqBody.RefText)
+
+	// output success response
+	api.WriteAPIResp(w, api.NewSuccessResp(map[string]interface{}{
+		"matches": matches,
+	}))
+}
+
 func InitHandler() {
 	http.Handle("/", HomeRoot())
-	http.HandleFunc("/analysis", func(w http.ResponseWriter, r *http.Request) {
-		// check http method
-		if r.Method != http.MethodPost {
-			api.WriteAPIResp(w, api.NewErrorResp(errors.NewErrMethodNotAllowed()))
-			return
-		}
-
-		// parse request body
-		var reqBody api.AnalyzeReqBody
-		err := json.NewDecoder(r.Body).Decode(&reqBody)
-		if err != nil {
-			api.WriteAPIResp(w, api.NewErrorResp(errors.NewErrBadRequest(err.Error())))
-			return
-		}
-
-		// validate request body
-		err = reqBody.Validate()
-		if err != nil {
-			api.WriteAPIResp(w, api.NewErrorResp(err))
-			return
-		}
-
-		// do analysis
-		matches := plagiarism.Analyze(reqBody.InputText, reqBody.RefText)
-
-		// output success response
-		api.WriteAPIResp(w, api.NewSuccessResp(map[string]interface{}{
-			"matches": matches,
-		}))
-	})
+	http.HandleFunc("/analysis", AnalysisRoot)
 }
